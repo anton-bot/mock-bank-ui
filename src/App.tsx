9a008ddc0278721fe5216d006eb96189ed0ca720
Anton Ivanov <anton@ivanov.hk>
2021-05-25 23:46:01 +0800

    chore: install prettier to maintain consistent code formatting helps avoid arguing over code style

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      It works.
-    </div>
-  );
+  return <div className="App">It works.</div>;
 }
 
 export default App;
